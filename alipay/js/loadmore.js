131e8c953ff6e11aed7807507025b5f24120e01d
yueyeuniao0528 <2276183000@qq.com>
2019-01-19 17:45:35 +0800

    shixun

---
+++ alipay/js/loadmore.js
@@ -0,0 +1,104 @@
+let step = 1;
+let messages =[];
+
+$(function () {
+
+    //请求   // 初始化加载
+    $.getJSON("../data/message.json" , function (results) {
+        messages = results;
+        console.log(results)
+    });
+
+    //生成页面
+    function createItem(arr) {           //根据传入的数组内容   去渲染 item
+
+        arr.forEach(function (e) {
+
+            // console.log(e)
+
+            //替换成 自己的 HTML ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
+            // let item = $("<div class=\"item\">\n" +
+            //     "            <div class=\"photo\"><img src="+e.photo+" height=\"60px\" width=\"60px\"/> </div>\n" +
+            //     "            <div class=\"\">\n" +
+            //     "                <div class=\"name\">"+e.name+"</div>\n" +
+            //     "                <div class=\"content\">"+e.content+"</div>\n" +
+            //     "            </div>\n" +
+            //     "        </div>");
+            //⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
+            //*********************************************
+            let item = $("<div class=\"dynamic4-bottoma  flew-flow2\">\n" +
+                "            <div class=\"dynamic4-bottomal  al-center\">\n" +
+                "                <div class=\"dynamic4-bottomalima  al-picture dynamic4-bottomalimgcl1\"\n" +
+                "                  style=\"background-image: url("+e.photo+")\">\n" +
+                "\n" +
+                "                </div>\n" +
+                "            </div>\n" +
+                "            <div class=\"dynamic4-bottomac dynamic4-bottomaborder\">\n" +
+                "                <div class=\"dynamic4-bottomacziti1\">\n" +
+                "                    "+e.name+"\n" +
+                "                </div>\n" +
+                "                <div class=\"dynamic4-bottomacziti2\">\n" +
+                "                    "+e.content+"\n" +
+                "                </div>\n" +
+                "\n" +
+                "            </div>\n" +
+                "            <div class=\"dynamic4-bottomar dynamic4-bottomaborder\">\n" +
+                "                <div class=\"dynamic4-bottomacziti3\">\n" +
+                "                    "+e.time+"\n" +
+                "                </div>\n" +
+                "            </div>\n" +
+                "        </div>");
+
+            //*********************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            $("#dynamic").append(item);
+        })
+
+    }
+
+//监听滚动
+    $("#dynamic").scroll(function () {   //监听滚动事件..
+
+
+        let container_totalHeight = this.scrollHeight;  //总高度
+
+        let contianer_scroll = $(this).scrollTop();   //卷动高度 // 看不见的部分
+
+        let container_height = $(this).height();   //元素可见部分的高度
+
+
+
+        if ((Math.ceil(contianer_scroll) + Math.ceil(container_height)) / container_totalHeight >0.9999 ) {
+
+            setTimeout(function () {
+                let arr = messages.slice((step-1) , step+4);   //splice 在原数组基础上删除某些元素 , slice 修改数组并返回副本 , 原本不动
+                createItem(arr);
+                step += 4;
+
+            }, 0)
+
+            // console.log("container_totalHeight =  " + container_totalHeight + " ,  container_height : " + container_height + " + scroll : "+contianer_scroll );
+
+        }
+
+    })
+
+
+
+});
